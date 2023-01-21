3a26855481514449dababa042a5285da9573be22
SHAHNAB AHMED <31092371+Shahnab@users.noreply.github.com>
2023-01-22 00:31:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import streamlit.components.v1 as components
 st. set_page_config(layout="wide")
 
 st.sidebar.image("https://www.protoclea.com/wp-content/uploads/2019/10/Automatic-number-plate-recognition-icon.png", width=100)
-st.sidebar.subheader("Models Used")
-st.sidebar.markdown("###### YoloV7: Object detection model to detect the number plate")
-st.sidebar.markdown("###### PaddleOCR: OCR to read the number plate")
-st.sidebar.markdown("###### Deep SORT: Object tracking algorithm for video detection")
-
+st.sidebar.subheader("About the Application")
 st.sidebar.markdown("###### The YOLOV7 Model was fine-tuned using the ANPR dataset to detect number plates. When a number plate is detected, PaddleOCR is used to read the number plate. For video detection, Deep SORT is used to handle object tracking")
+
+st.sidebar.subheader("Models Used")
+st.sidebar.markdown("###### - YoloV7: Object detection model to detect the number plate")
+st.sidebar.markdown("###### - PaddleOCR: OCR to read the number plate")
+st.sidebar.markdown("###### - Deep SORT: Object tracking algorithm for video detection")
 
 st.sidebar.caption("Streamlit App by </Shahnab>")
 
